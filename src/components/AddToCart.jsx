1285cd1d921bfdd007486ec45762e55544f148ef
cristian prieto <cristianemanuelprieto@gmail.com>
2022-08-24 00:03:19 +0200

    Co-authored-by: Julián Prieto <herbie.juls@gmail.com>

---
+++ src/components/AddToCart.jsx
@@ -0,0 +1,36 @@
+import { useAppContext } from "../hooks/useAppContext";
+export function AddToCart(product) {
+  const { removeProductFromCart, addProductToCart, cart } = useAppContext();
+  const productInCart = cart.find((item) => item.product.id === product.id);
+
+  return (
+    <>
+      {productInCart ? (
+        <div className="flex items-center gap-4">
+          <button
+            onClick={() => removeProductFromCart(productInCart)}
+            className="mt-auto px-4 py-2 uppercase shadow-md rounded-xl bg-white transition hover:bg-red-600 hover:text-gray-100 disabled:opacity-30 disabled:hover:bg-white"
+          >
+            - 1
+          </button>
+          <span className="flex w-8 font-semibold justify-center">
+            {productInCart.count}
+          </span>
+          <button
+            onClick={() => addProductToCart(product)}
+            className="mt-auto px-4 py-2 uppercase shadow-md rounded-xl bg-white transition hover:bg-green-400 hover:text-gray-100 disabled:opacity-30 disabled:hover:bg-white"
+          >
+            + 1
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={() => addProductToCart(product)}
+          className="mt-auto px-4 py-2 uppercase shadow-md rounded-xl bg-white transition hover:bg-yellow-300 disabled:opacity-30 disabled:hover:bg-white"
+        >
+          Add to cart
+        </button>
+      )}
+    </>
+  );
+}
