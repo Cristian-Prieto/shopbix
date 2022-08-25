c9e848cd1cb76189f2a672567ef9b3511d39898b
cristian prieto <cristianemanuelprieto@gmail.com>
2022-08-25 23:17:02 +0200

    form styled

--- src/components/EditProduct.jsx
+++ src/components/EditProduct.jsx
@@ -18,7 +18,8 @@ export function EditProduct() {
     setNewProduct({ ...newProduct, [event.target.name]: event.target.value });
   };
 
-  const addProductUrl = "https://api.escuelajs.co/api/v1/products/";
+  const addProductUrl =
+    "https://fake-api-shopping-backend.onrender.com/api/v1/products";
   const addNewProduct = (event) => {
     event.preventDefault();
     fetch(addProductUrl, {
@@ -40,48 +41,67 @@ export function EditProduct() {
         onSubmit={addNewProduct}
         className="flex flex-col w-96 p-8 shadow-lg rounded-lg bg-white"
       >
-        <label className="flex flex-col">
-          Title
+        <div className="relative z-0 mb-6 w-full group">
           <input
             type="text"
             name="title"
+            id="title"
             value={newProduct.title}
             onChange={handleInputChange}
-            className="border-black rounded-md focus:border-yellow-400 focus:ring-yellow-400 "
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-yellow-400 peer"
+            placeholder=" "
+            required
           />
-        </label>
-        <label className="flex flex-col">
-          Price
+          <label
+            htmlFor="title"
+            className="peer-focus:font-medium absolute text-sm text-gray-700  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-yellow-400  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Title
+          </label>
+        </div>
+
+        <div className="relative z-0 mb-6 w-full group">
           <input
             type="number"
-            min={1}
             name="price"
             value={newProduct.price}
             onChange={handleInputChange}
-            className="rounded-md focus:border-yellow-400 focus:ring-yellow-400 invalid:border-red-500"
+            id="price"
+            className="block py-2.5 px-0 w-full text-sm text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-yellow-400 peer"
+            placeholder=" "
+            required
           />
-        </label>
-        <label className="flex flex-col">
-          Description
+          <label
+            htmlFor="price"
+            className="peer-focus:font-medium absolute text-sm text-gray-800  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-yellow-400  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Price
+          </label>
+        </div>
+
+        <div className="relative z-0 mb-6 w-full group">
           <textarea
             type="text"
-            autoComplete="none"
             name="description"
+            id="description"
             value={newProduct.description}
             onChange={handleInputChange}
-            className="rounded-md h-20 break-all focus:border-yellow-400 focus:ring-yellow-400 invalid:border-red-500"
+            className="block py-2.5 px-0 w-full text-sm text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-yellow-400 peer"
+            placeholder=" "
+            required
           />
-        </label>
-        {/* <label>
-          Category
-          <InputText
-            type="text"
-            name="categoryId"
-            value={newProduct.categoryId}
-            onChange={handleInputChange}
-          />
-        </label> */}
-        <button type="submit" className="p-4">
+          <label
+            htmlFor="description"
+            className="peer-focus:font-medium absolute text-sm text-gray-800  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-yellow-400  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Description
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          className="mt-4 px-4 py-2 uppercase shadow-md rounded-xl bg-white transition hover:bg-yellow-300 disabled:opacity-30 disabled:hover:bg-white"
+        >
           New Product
         </button>
       </form>
