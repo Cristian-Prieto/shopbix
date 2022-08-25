d5e5db56a1d7a47bfa0ab5ad407f8ff14f3cd69b
cristian prieto <cristianemanuelprieto@gmail.com>
2022-08-25 23:21:22 +0200

    final touch form

--- src/components/EditProduct.jsx
+++ src/components/EditProduct.jsx
@@ -75,7 +75,7 @@ export function EditProduct() {
             htmlFor="price"
             className="peer-focus:font-medium absolute text-sm text-gray-800  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-yellow-400  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Price
+            Price (USD)
           </label>
         </div>
 
